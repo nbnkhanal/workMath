64264c8bbbeebfaccc03260c4ea8649880d6485a
Nabin <mario@Nabins-MacBook-Pro.local>
2020-04-16 23:55:43 -0500

    regexValidator class cleanup

--- src/main/java/com/example/workMath/util/RegexValidator.java
+++ src/main/java/com/example/workMath/util/RegexValidator.java
@@ -1,21 +1,38 @@
 package com.example.workMath.util;
 
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Properties;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 //@Service
-@Configuration
+//@Configuration
+@Component
 public class RegexValidator {
-	
+	String username = "";
+	InputStream inputStream;
 	public String regexProp() throws Exception{
 	
-	FileInputStream fis = new FileInputStream("/Users/mario/myFolder/projects/workMath/src/main/resources/regex.properties");
+	try {
 	Properties props = new Properties();
-	props.load(fis);
-	String username = props.getProperty("namePattern");
-	System.out.println(username);
+	String propFileName = "regex.properties";	
+	inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+	 
+	if (inputStream != null) {
+		props.load(inputStream);
+	} else {
+		throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+	}
+	String regexValue = props.getProperty("namePattern");
+	username = regexValue;
+	System.out.println("regex value is -------><>"+username);
+	} catch (Exception e) {
+		System.out.println("Exception: " + e);
+	} finally {
+		inputStream.close();
+	}
+	System.out.println("uname is ------> " + username);
 	return username;
 	}
 	
